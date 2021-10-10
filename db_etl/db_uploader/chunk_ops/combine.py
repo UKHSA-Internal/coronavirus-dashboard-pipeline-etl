fc650a71df33e87ebe0686650c7cfd81a666de13
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-10 23:56:56 +0100

    comments out deprecated sections / highlights as removal candidate

--- db_etl/db_uploader/chunk_ops/combine.py
+++ db_etl/db_uploader/chunk_ops/combine.py
@@ -1,5 +1,8 @@
 #!/usr/bin python3
 
+# ToDo:
+#   Candidate for removal. Investigate dependencies.
+
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Python:
@@ -28,7 +31,7 @@ __version__ = "0.0.1"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 __all__ = [
-    'combine_and_upload_from_file'
+    # 'combine_and_upload_from_file'
 ]
 
 
@@ -50,7 +53,7 @@ UPLOAD_ERROR_MSG = (
 )
 
 
-db_container = CosmosDB(Collection.DATA, writer=True)
+# db_container = CosmosDB(Collection.DATA, writer=True)
 
 
 async def download_file(container: str, path: str) -> DataFrame:
@@ -90,73 +93,73 @@ async def iter_data(data: DataFrame):
         yield row
 
 
-async def upsert(row, nested_column_names):
-    for col in nested_column_names:
-        row[col] = row[col] or []
-
-    row_data = dict(filter(lambda x: x[1] is not None, row.items()))
-
-    attempt = 0
-
-    while attempt < UPLOAD_RETRIES:
-        try:
-            db_container.upsert(row_data)
-            return None
-        except Exception as e:
-            logging.warning(e)
-            attempt += 1
-            await sleep(attempt * 2)
-
-    logging.critical(UPLOAD_ERROR_MSG.format(**row_data))
-
-
-async def combine_and_upload_from_file(container: str, date: str, area_type: str, area_code: str):
-    logging.info(f"--- Starting the process to combined and upload data from ")
-
-    loop = get_event_loop()
-
-    main_path = MAIN_DATA.format(
-        date=date,
-        area_type=area_type,
-        area_code=area_code
-    )
-    data = await get_data(container, main_path)
-    # data.drop(columns=['index'], inplace=True)
-
-    tasks = list()
-    for template_path in SUPPLEMENTS:
-        filepath = template_path.format(
-            date=date,
-            area_type=area_type,
-            area_code=area_code
-        )
-        tmp = get_data(container, filepath)
-        task = loop.create_task(tmp)
-        tasks.append(task)
-
-    column_names = set()
-    done, pending = await wait(tasks)
-    for future in done:
-        supplement = future.result()
-        column_names = column_names.union(supplement.columns)
-        data = data.join(supplement, on=INDEX_METRICS, how='left')
-
-    column_names = column_names.difference(INDEX_METRICS)
-
-    data.reset_index(inplace=True)
-    data = data.where(data.notnull(), None)
-
-    async for row in iter_data(data):
-        loop.create_task(upsert(row, column_names))
+# async def upsert(row, nested_column_names):
+#     for col in nested_column_names:
+#         row[col] = row[col] or []
+#
+#     row_data = dict(filter(lambda x: x[1] is not None, row.items()))
+#
+#     attempt = 0
+#
+#     while attempt < UPLOAD_RETRIES:
+#         try:
+#             db_container.upsert(row_data)
+#             return None
+#         except Exception as e:
+#             logging.warning(e)
+#             attempt += 1
+#             await sleep(attempt * 2)
+#
+#     logging.critical(UPLOAD_ERROR_MSG.format(**row_data))
 
 
-if __name__ == '__main__':
-    eloop = get_event_loop()
-    eloop.run_until_complete(
-        combine_and_upload_from_file(
-            container="pipeline",
-            date="2020-12-23",
-            area_type="nation",
-            area_code="E92000001"
-        )
-    )
+# async def combine_and_upload_from_file(container: str, date: str, area_type: str, area_code: str):
+#     logging.info(f"--- Starting the process to combined and upload data from ")
+#
+#     loop = get_event_loop()
+#
+#     main_path = MAIN_DATA.format(
+#         date=date,
+#         area_type=area_type,
+#         area_code=area_code
+#     )
+#     data = await get_data(container, main_path)
+#     # data.drop(columns=['index'], inplace=True)
+#
+#     tasks = list()
+#     for template_path in SUPPLEMENTS:
+#         filepath = template_path.format(
+#             date=date,
+#             area_type=area_type,
+#             area_code=area_code
+#         )
+#         tmp = get_data(container, filepath)
+#         task = loop.create_task(tmp)
+#         tasks.append(task)
+#
+#     column_names = set()
+#     done, pending = await wait(tasks)
+#     for future in done:
+#         supplement = future.result()
+#         column_names = column_names.union(supplement.columns)
+#         data = data.join(supplement, on=INDEX_METRICS, how='left')
+#
+#     column_names = column_names.difference(INDEX_METRICS)
+#
+#     data.reset_index(inplace=True)
+#     data = data.where(data.notnull(), None)
+#
+#     async for row in iter_data(data):
+#         loop.create_task(upsert(row, column_names))
+
+
+# if __name__ == '__main__':
+#     eloop = get_event_loop()
+#     eloop.run_until_complete(
+#         combine_and_upload_from_file(
+#             container="pipeline",
+#             date="2020-12-23",
+#             area_type="nation",
+#             area_code="E92000001"
+#         )
+#     )
