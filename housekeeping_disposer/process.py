3dec31cee7cd78a18179a047b076429ee68b6ac6
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-14 15:38:01 +0100

    integrates manifest + dtypes

--- housekeeping_disposer/process.py
+++ housekeeping_disposer/process.py
@@ -4,16 +4,21 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Python:
 import logging
+from typing import List
 
 # 3rd party:
 
 # Internal: 
 try:
     from __app__.storage import StorageClient
-    from __app__.housekeeping_orchestrator.dtypes import DisposerPayload, GenericPayload
+    from __app__.housekeeping_orchestrator.dtypes import (
+        ArtefactPayload, GenericPayload, DisposerResponse
+    )
 except ImportError:
     from storage import StorageClient
-    from housekeeping_orchestrator.dtypes import DisposerPayload, GenericPayload
+    from housekeeping_orchestrator.dtypes import (
+        ArtefactPayload, GenericPayload, DisposerResponse
+    )
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -22,7 +27,7 @@ __all__ = [
 ]
 
 
-def main(payload: GenericPayload):
+def main(payload: GenericPayload) -> DisposerResponse:
     """
     Removes blobs from the storage.
 
@@ -32,20 +37,23 @@ def main(payload: GenericPayload):
 
     Returns
     -------
-    str
+    DisposerResponse
         Message confirming that the process is done.
     """
-    payload_content: DisposerPayload = payload['content']
-    logging.info(f"Triggered - total blobs to remove: {len(payload['content'])}")
+    logging.info(f"triggered with manifest: {payload['manifest']}")
+    logging.info(f"- total blobs to remove: {len(payload['tasks'])}")
 
-    first_path = payload_content['removables'][0]
-    with StorageClient(container="pipeline", path=first_path) as cli:
+    payload_content: List[ArtefactPayload] = payload['tasks']
+
+    first_path = payload_content[0]['from_path']
+    with StorageClient(container=payload['manifest']['container'], path=first_path) as cli:
         container = cli.get_container()
-        for path in payload_content['removables']:
-            container.delete_blob(path)
+
+        for artefact in payload_content:
+            container.delete_blob(artefact['from_path'])
 
         container.close()
 
-    logging.info(f"Done: {payload['timestamp']}")
+    logging.info(f"done: {payload['timestamp']}")
 
-    return {"total_processed": len(payload['content']['removables'])}
+    return DisposerResponse(total_processed=len(payload['tasks']))
