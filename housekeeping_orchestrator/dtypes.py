805641170527ed4889153465fcfd0b0836e9c60b
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-10-14 15:34:45 +0100

    implements new dtypes + refactoring

--- housekeeping_orchestrator/dtypes.py
+++ housekeeping_orchestrator/dtypes.py
@@ -1,36 +1,61 @@
 #!/usr/bin python3
 
 from typing import TypedDict, Union, List
+from enum import Enum
 
 
 __all__ = [
     "ArtefactPayload",
     "RetrieverPayload",
-    "DisposerPayload",
-    "GenericPayload"
+    "GenericPayload",
+    "Manifest",
+    "ProcessMode",
+    "MetaDataManifest",
+    'DisposerResponse'
 ]
 
 
-class DisposerPayload(TypedDict):
-    removables: List[str]
-    date: str
+class ProcessMode(Enum):
+    ARCHIVE_AND_DISPOSE = 0
+    ARCHIVE_ONLY = 1
+    DISPOSE_ONLY = 2
+
+
+class Manifest(TypedDict):
+    label: str
+    container: str
+    directory: str
+    regex_pattern: str
+    date_format: str
+    offset_days: int
+    archive_directory: Union[str, None]
+    mode: ProcessMode
 
 
 class ArtefactPayload(TypedDict):
     date: str
-    category: str
-    subcategory: Union[str, None]
     filename: str
     from_path: str
     content_type: str
 
 
+class MetaDataManifest(TypedDict):
+    original_artefact: ArtefactPayload
+    archive_path: str
+
+
 class RetrieverPayload(TypedDict):
     timestamp: str
     environment: str
+    manifest: Manifest
+
+
+class DisposerResponse(TypedDict):
+    total_processed: int
 
 
 class GenericPayload(TypedDict):
     timestamp: str
     environment: str
-    content: Union[List[ArtefactPayload], RetrieverPayload, DisposerPayload]
+    tasks: Union[List[ArtefactPayload], RetrieverPayload]
+    manifest: Manifest
