1c2ce9412ffe74b42805d9381724b59a1e3d8ecc
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-02 12:20:11 +0000

    implements cache buster for despatch

---
+++ despatch_ops_cache_buster/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin python3
+
+"""
+Makes arrangements to update cache for despatch.
+
+Author:        Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
+Created:       02 Nov 2021
+License:       MIT
+Contributors:  Pouria Hadjibagheri
+"""
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+
+# 3rd party:
+
+# Internal: 
+from .process import *
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Header
+__author__ = "Pouria Hadjibagheri"
+__copyright__ = "Copyright (c) 2021, Public Health England"
+__license__ = "MIT"
+__version__ = "2.5.1"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ despatch_ops_cache_buster/process.py
@@ -0,0 +1,67 @@
+#!/usr/bin python3
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+from typing import Iterator
+from enum import IntEnum
+
+# 3rd party:
+
+# Internal:
+try:
+    from __app__.caching import RedisClient
+except ImportError:
+    from caching import RedisClient
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__all__ = [
+    'get_operations',
+    'main'
+]
+
+
+class CacheDatabase(IntEnum):
+    APIM = 0
+    GREENHOUSE = 1
+    SUMMARY = 2
+    GENERIC_API = 3
+
+
+# First level key is the DB number.
+# Second level key is a `RedisClient` method name.
+# Third level key is the payload for the method.
+CACHE_OPERATIONS = {
+    CacheDatabase.GENERIC_API: {
+        "delete_pattern": {
+            "key_pattern": [
+                "*v[12]/data*",
+                "*/generic/soa/msoa/*"
+            ]
+        },
+    },
+    CacheDatabase.SUMMARY: {
+        "delete_pattern": {
+            "key_pattern": [
+                "[^area]*"
+            ]
+        }
+    }
+}
+
+
+def get_operations() -> Iterator:
+    for db, operations in CACHE_OPERATIONS.items():
+        yield {"db": db, "operations": operations}
+
+
+def main(payload):
+    with RedisClient(db=payload['db']) as cli:
+        for method, payload in payload['operations'].items():
+            func = getattr(cli, method)
+            if isinstance(payload, (list, str, int, float)):
+                func(payload)
+            else:
+                # It's a dict
+                func(**payload)
