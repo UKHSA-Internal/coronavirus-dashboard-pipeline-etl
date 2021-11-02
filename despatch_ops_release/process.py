6be91432ae913f5100c6e77a389888c4f4de34b5
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-02 12:20:29 +0000

    implements timestamp updater for despatch

---
+++ despatch_ops_release/process.py
@@ -0,0 +1,72 @@
+#!/usr/bin python3
+
+"""
+<Description of the programme>
+
+Author:        Pouria Hadjibagheri <pouria.hadjibagheri@phe.gov.uk>
+Created:       02 Nov 2021
+License:       MIT
+Contributors:  Pouria Hadjibagheri
+"""
+
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python:
+
+# 3rd party:
+
+# Internal: 
+try:
+    from __app__.storage import StorageClient
+except ImportError:
+    from storage import StorageClient
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Header
+__author__ = "Pouria Hadjibagheri"
+__copyright__ = "Copyright (c) 2021, Public Health England"
+__license__ = "MIT"
+__version__ = "0.0.1"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+__all__ = [
+    "ReleaseTimestamps",
+    "main"
+]
+
+
+ReleaseTimestamps = [
+    {
+        "process": lambda ts: ts.split("T")[0],
+        "path": "info/seriesDate",
+        "container": "pipeline"
+    },
+    {
+        "process": lambda ts: f"{ts}Z",
+        "path": "assets/dispatch/website_timestamp",
+        "container": "publicdata"
+    },
+    {
+        "process": lambda ts: f"{ts}5Z",
+        "path": "info/latest_published",
+        "container": "pipeline",
+    }
+]
+
+
+UPLOAD_KWS = dict(
+    content_type="text/plain; charset=utf-8",
+    cache="no-cache, max-age=0",
+    compressed=False
+)
+
+
+def main(payload):
+    kws = {
+        **UPLOAD_KWS,
+        **payload
+    }
+    value = kws.pop("value")
+
+    with StorageClient(**kws) as client:
+        client.upload(value)
