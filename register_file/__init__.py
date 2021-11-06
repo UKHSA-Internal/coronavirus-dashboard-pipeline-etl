59d4cb96387e7252dc7f72766fe832f40f22a881
Pouria Hadjibagheri <p.h.bagheri@gmail.com>
2021-11-06 10:52:06 +0000

    version bump + refactoring

--- register_file/__init__.py
+++ register_file/__init__.py
@@ -23,5 +23,5 @@ from .process import *
 __author__ = "Pouria Hadjibagheri"
 __copyright__ = "Copyright (c) 2021, Public Health England"
 __license__ = "MIT"
-__version__ = "3.0.1"
+__version__ = "3.0.2"
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
