86ad2cf00383abbbd674becd9a74eb2ed8f75040
Jaroslaw Michalski <jaroslaw.m.michalski@phe.gov.uk>
2023-05-22 11:18:02 +0100

    generate spring boosters waffle chart
    Changed autumn22 boosters metrics to spring23

--- db_etl_homepage_graphs/tests/test_waffle_chart.py
+++ db_etl_homepage_graphs/tests/test_waffle_chart.py
@@ -9,8 +9,8 @@ import logging
 import os
 import unittest
 
-from db_etl_homepage_graphs.grapher import get_value_50_plus
-from .test_data.db_data import VACCINATIONS_QUERY_50_PLUS
+from db_etl_homepage_graphs.grapher import get_value_75_plus
+from db_etl_homepage_graphs.tests.test_data.db_data import VACCINATIONS_QUERY_PLUS
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -22,18 +22,19 @@ class TestWaffleCharts(unittest.TestCase):
             "area_type": "nation",
             "area_code": "E92000001",
             "date": "2022-11-17",
-            'payload': VACCINATIONS_QUERY_50_PLUS,
+            'payload': VACCINATIONS_QUERY_PLUS,
         }
         return super().setUp()
 
     def tearDown(self) -> None:
         return super().tearDown()
 
-    def test_get_value_50_plus(self):
-        result = get_value_50_plus(self.data)
+    def test_get_value_75_plus(self):
+        result = get_value_75_plus(self.data)
+        print(result)
         
-        self.assertEqual(result['vaccination_date'], 10866622)
-        self.assertEqual(result['vaccination_date_percentage_dose'], 49)
+        self.assertEqual(result['vaccination_date'], 4178999)
+        self.assertEqual(result['vaccination_date_percentage_dose'], 48)
 
 
 if __name__ == '__main__':
